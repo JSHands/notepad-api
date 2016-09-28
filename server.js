aa2e5301f0e03ce197850e8be1e3209ac7b4da0a
DecaHub <dan@decahub.com>
2016-09-28 15:34:15 -0400

    Promisify existing routes to apply DRY

--- server.js
+++ server.js
@@ -1,87 +1,134 @@
 "use strict";
 
-let express = require('express');
-let fs = require('fs');
-let bodyParser = require('body-parser');
-
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const path = require('path');
+	
 
 let app = express();
+let dataPath = '/data/notes.json';
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
-res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-res.set('Content-Type', 'application/json');
-next();
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.set('Content-Type', 'application/json');
+	next();
 });
+
+// Utilties
+
+let logSendError = (res, err) => {
+	res.status(500).end();
+	return console.log(err);
+};
+
+let readDataAsync = () => {
+	return new Promise((resolve, reject)  => {
+		fs.readFile(path.join(__dirname, dataPath), 'utf8', (err, data) => {
+			if (err) {
+				reject(err)
+			} else {
+				resolve(data)
+			}
+		});
+	})
+};
+
+let writeSendData = (res, data) => {
+	writeDataAsync(data).then((data) => {
+		res.status(200).send(data);
+	}).catch((err) => {
+		logSendError(res, err);
+	});
+};
+
+let writeDataAsync = (data) => {
+	return new Promise((resolve, reject) => {
+		fs.writeFile(path.join(__dirname, dataPath), JSON.stringify(data), (err) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(data);
+			}
+		})
+	})
+};
+	
+	
+// Routes
 
 app.get('/', (req, res) => {
 	res.send('API is running.');
 });
 
 app.get('/notes', (req,  res) => {
-	fs.readFile(__dirname + '/data/notes.json', (err, data) => {
-	if (err) {
-		res.status(500).end();
-		return console.log(err);
-	}
 	
-	res.status(200).send(data);
-});
+	readDataAsync().
+		then((data) => {
+			res.status(200).send(data);
+	}).
+		catch((error) => {
+			logSendError(res, err);
+	});
+	
 });
 
 // Update a note
 
 app.put('/notes/:id', (req, res) => {
-	fs.readFile(__dirname + '/data/notes.json', 'utf8', (err, data) => {
 	
-	console.log("putting note!");
-
-if (err) {
-	res.status(500).end();
-	return console.log(err);
-}
-
-try {
-	data = JSON.parse(data);
-} catch (e) {
-	res.status(500).end();
-	return console.log(e);
-}
-
-data.forEach((note, index) => {
-	if (note.id == req.params.id) {
-	data[index] = req.body;
-}
-});
-
-fs.writeFile(__dirname + '/data/notes.json', JSON.stringify(data), (err) => {
-	if (err) {
-		res.status(500).end();
-		return console.log(err);
-	}
-	
-	res.status(200).send(data);
-	
-});
-	
-});
-	
+	readDataAsync().
+		then((data) => {
+		
+			let match = false;
+			
+			try {
+				data = JSON.parse(data);
+			} catch (e) {
+				logSendError(res, e);
+			}
+		
+			data.forEach((note, index) => {
+				if (note.id == req.params.id) {
+					data[index] = req.body;
+					match = true;
+				}
+			});
+		
+			if (!match) {
+				logSendError(res, "No existing note found to be updated.");
+			}
+			
+			writeSendData(res,  data);
+		
+	}).
+		catch((err) => {
+		logSendError(res,  err);
+	});
 });
 
 // Create a new note
 
 app.post('/notes', (req, res) => {
 	
-	fs.readFile(__dirname + '/data/notes.json', 'utf8', (err, data) => {
-	
-	if (err) {
-		res.status(500).end();
-	}
-	
-})
-	
+	readDataAsync().then((data) => {
+		
+		try {
+			data = JSON.parse(data);
+		} catch (e) {
+			logSendError(res, e);
+		}
+		
+		data.push(req.body);
+		
+		writeSendData(res,  data);
+		
+	}).catch((err) => {
+		logSendError(res, err);
+	});
 });
 
 app.listen(5000, () => {
